4f0100cb46a024f4b5cd05a0a0f5c1342ef4d5e8
Mostafa Elemam <m.emam@intrazero.net>
2025-06-25 13:49:36 +0300

    [Feature] [UI] Added Experience View and custom Fonts.

---
+++ AroundEgyptTask/Extensions/View+Extension.swift
@@ -0,0 +1,17 @@
+//
+//  View+Extension.swift
+//  AroundEgyptTask
+//
+//  Created by Mostafa Elemam on 25/06/2025.
+//
+
+import Foundation
+
+import SwiftUI
+
+extension View {
+    
+    func customFont(_ font: GothamFont, size: CGFloat) -> some View {
+        modifier(CustomFontModifier(font: font, size: size))
+    }
+}
---
+++ AroundEgyptTask/Utilities/GothamFont.swift
@@ -0,0 +1,15 @@
+//
+//  GothamFont.swift
+//  AroundEgyptTask
+//
+//  Created by Mostafa Elemam on 25/06/2025.
+//
+
+import Foundation
+
+enum GothamFont: String {
+    case regular     = "Gotham-Regular"
+    case medium      = "Gotham-Medium"
+    case bold        = "Gotham-Bold"
+    case boldRounded = "GothamRnd-Bold"
+}
