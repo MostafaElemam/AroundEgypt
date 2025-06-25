ee916e617ad44e5f4ea2c237fbd24bdf5d2194a6
Mostafa Elemam <m.emam@intrazero.net>
2025-06-25 11:33:24 +0300

    Initial Commit

---
+++ AroundEgyptTask/AroundEgyptTaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  AroundEgyptTaskApp.swift
+//  AroundEgyptTask
+//
+//  Created by Mostafa Elemam on 25/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct AroundEgyptTaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
